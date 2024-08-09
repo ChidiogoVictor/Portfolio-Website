a5b66496914369657ceebad5692c3f0a7945e734
ChidiogoVictor <chidiogovictor10@gmail.com>
2024-08-09 12:35:46 +0100

    style project card

--- src/Components/WorkCardData.js
+++ src/Components/WorkCardData.js
@@ -5,21 +5,21 @@ import pro3 from "../Assets/pro3.png"
 const WorkCardData = [
     {
         imgsrc: pro1,  
-        title: "Ecommerce Website Design",
+        title: "E-commerce Website",
         text: " This is a marketing website used for purchase of electronic devices, it makes it easy for customers to have direct access to foreign products as well.",
         view: "https://www.google.com"
     },
 
     {
         imgsrc: pro2,  
-        title: "Programming Website Design",
+        title: "Programming Website",
         text: " This is a Programming Company website used for communicating with clients, for customers to have direct access to developers and make requests.",
         view: "https://www.google.com"
     },
 
     {
         imgsrc: pro3,  
-        title: "Crypto Investment Website Design",
+        title: "Crypto Investment Website",
         text: " This is an Investment  website for crypto currencies, it is also used for buying and selling Bitcoin, USDT, Etherum and other crypto currencies.",
         view: "https://www.google.com"
     }
